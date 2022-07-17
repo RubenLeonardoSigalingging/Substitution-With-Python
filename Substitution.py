2444a41bb31d4ad13730763cd6419464bd5c8d93
Ruben Leonardo Sigalingging <106874389+RubenLeonardoSigalingging@users.noreply.github.com>
2022-07-17 23:58:11 +0700

    Add files via upload

---
+++ Substitution.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+VarA,VarB=3,9
+print(f"VarA: {VarA}\nVarB: {VarB}\n")
+
+
+# substitution
+VarA=9
+VarB=3
+print("After Substitution\n")
+print(f"VarA: {VarA}\nVarB: {VarB}\n")
+
+
+VarD=5
+VarE=8
+VarC=VarE+VarB+1
+print(f"VarC: {VarC}")
+print(f"VarF: {VarD+VarE}")
